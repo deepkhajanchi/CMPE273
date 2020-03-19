b3ddadebf111ad2568ffa610bdaeabd953798768
deepkhajanchi <45371064+deepkhajanchi@users.noreply.github.com>
2020-03-19 04:38:24 -0700

    Create students

---
+++ Handshake/Frontend/src/components/Student/createStudent.js
@@ -0,0 +1,134 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import { addStudent } from '../../js/actions/index';
+
+class CreateStudents extends Component {
+  constructor(props) {
+    super(props);
+
+    axios.get('/companies/user')
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.isCompany) {
+          window.location.href = '/company/landing';
+        }
+      });
+    axios.get('/students/user')
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.isStudent) {
+          window.location.href = '/student/landing';
+        }
+      });
+    // Functions
+    this.onChangeStudentName = this.onChangeStudentName.bind(this);
+    this.onChangeStudentEmail = this.onChangeStudentEmail.bind(this);
+    this.onChangeStudentPassword = this.onChangeStudentPassword.bind(this);
+    this.onChangeStudentCollegeName = this.onChangeStudentCollegeName.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    // State
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      collegeName: '',
+    };
+  }
+
+  onChangeStudentName(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  onChangeStudentEmail(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  onChangeStudentPassword(e) {
+    this.setState({ password: e.target.value });
+  }
+
+  onChangeStudentCollegeName(e) {
+    this.setState({ collegeName: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const {
+      name, email, password, collegeName,
+    } = this.state;
+    const {
+      addStudent: add,
+    } = this.props;
+    const studentObject = {
+      name,
+      email,
+      password,
+      collegeName,
+    };
+    add(studentObject);
+    this.setState({
+      name: '', email: '', password: '', collegeName: '',
+    });
+  }
+
+  render() {
+    const {
+      name, email, password, collegeName,
+    } = this.state;
+    return (
+      <div className="form-wrapper">
+        <h1>Students Sign Up</h1>
+        <Form onSubmit={this.onSubmit}>
+          <Form.Group controlId="Name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control required type="text" value={name} onChange={this.onChangeStudentName} />
+          </Form.Group>
+
+          <Form.Group controlId="Email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control required type="email" value={email} onChange={this.onChangeStudentEmail} />
+          </Form.Group>
+
+          <Form.Group controlId="Password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control required type="password" value={password} onChange={this.onChangeStudentPassword} />
+          </Form.Group>
+
+          <Form.Group controlId="CollegeName">
+            <Form.Label>College Name</Form.Label>
+            <Form.Control required type="text" value={collegeName} onChange={this.onChangeStudentCollegeName} />
+          </Form.Group>
+          
+          <Button variant="light" size="lg" block="block" type="submit">
+            Add Student
+          </Button>
+          <br />
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="/student-signin" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </Form>
+      </div>
+    );
+  }
+}
+CreateStudents.propTypes = {
+  addStudent: PropTypes.func.isRequired,
+};
+function mapDispatchToProps(dispatch) {
+  return {
+    addStudent: (student) => dispatch(addStudent(student)),
+  };
+}
+const CreateStudent = connect(null, mapDispatchToProps)(CreateStudents);
+export default CreateStudent;
